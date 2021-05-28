db4b6739836cbe5b23c06cf8325ab27ff86691f5
Lucie-Maya <luciemayabrosson@gmail.com>
2021-05-28 20:01:17 +0100

    modif

--- final project - Al the debater/Al_the_debater.py
+++ final project - Al the debater/Al_the_debater.py
@@ -9,16 +9,17 @@ import wikipediaapi
 # other library
 import random
 import emoji
-
-# To do
-# work with the study and statistic
-#or work with the other API and add picture and details on everything
-# send a message when someone join on how to play with the bot
+import giphy_client
+from giphy_client.rest import ApiException
 
 
+# API Gif giphy - to replace the image that I want I will you gif
+giphy_API_key = "put your api key here"
+giphy_API_instance = giphy_client.DefaultApi()
 
 # Token to access discord
-TOKEN = "Put your token here"
+TOKEN = "put your API key here"
+
 
 # define the bot command and it's prefix
 client = commands.Bot(command_prefix = 'al ')
@@ -51,26 +52,28 @@ async def on_member_remove(member):
 async def hello(ctx):
     await ctx.send(f'Well good day {ctx.author}, how may I assist you today')
 
-@client.command(name="status", help="gives you his")
+@client.command(name="status", help="gives you his status")
 async def status(ctx):
+
     status = ("Pretty good I should say","I... That is a good question, this is the first time someone ask. I would say better now that you asked","Terrible I have to say, I can't find the 3rd volume of my botanic encyclopedia")
 
     await ctx.send(random.choice(status))
 
-@client.command(name="statistic", help="help you with statistic")
-async def statistic(ctx):
-    await ctx.send('to be modified stats')
 
 @client.command(name="definition", help="helps you with definition")
 async def definition(ctx):
-    await ctx.send('to be modified definition')
+
 
     User_message = ctx.message.content
     wikipedia_research_definition = User_message[3:]
 
-    await ctx.send('Dear debater, here I my humble research on ' + wikipedia_research_definition + " :book:")
+    try :
+        await ctx.send('Dear debater, here I my humble research on ' + wikipedia_research_definition + " :book:")
+        await ctx.send('Definition : ' + wikipedia.summary(wikipedia_research_definition, sentences=3))
 
-    await ctx.send('Definition : ' + wikipedia.summary(wikipedia_research_definition, sentences=3))
+    except wikipedia.exceptions.PageError as a :
+
+        await ctx.send ("I am sorry my dear but this is out of my jurisdiction")
 
 
 @client.command(name="date", help="helps you with date")
@@ -79,20 +82,18 @@ async def date(ctx):
     User_message = ctx.message.content
     wikipedia_research_date = User_message[3:]
 
-    await ctx.send('Dear debater, here I my humble research on ' + wikipedia_research_date + " :moyai:")
+    try :
+        await ctx.send('Dear debater, here I my humble research on ' + wikipedia_research_date + " :moyai:")
 
-    await ctx.send('Description : ' + wikipedia.summary(wikipedia_research_date, sentences=3))
+        await ctx.send('Description : ' + wikipedia.summary(wikipedia_research_date, sentences=3))
 
-    date_events = wikipedia.page("Events" + wikipedia_research_date).section('Events','January-December','Date unknown')
-    date_cut = date_events[:200]
+        date_events = wikipedia.page("Events" + wikipedia_research_date).section('Events','January-December','Date unknown')
+        date_cut = date_events[:200]
 
-    await ctx.send(date_cut)
+        await ctx.send(date_cut)
+    except wikipedia.exceptions.PageError as a :
 
-
-@client.command(name="study", help="helps you with studies")
-async def study(ctx):
-    await ctx.send('to be modified study')
-
+        await ctx.send ("I am sorry my dear but this is out of my jurisdiction")
 
 
 @client.command(name="artwork", help="helps you with your art research")
@@ -101,31 +102,65 @@ async def artwork(ctx):
     User_message = ctx.message.content
     wikipedia_research_art = User_message[3:]
 
-    # add images with the poster find a way to get the right picture
-    await ctx.send(wikipedia.page(wikipedia_research_art).images[1])
+    try :
 
-    await ctx.send('Dear debater, here I my humble research on ' + wikipedia_research_art + " :art:")
-    await ctx.send('Description : ' + wikipedia.summary(wikipedia_research_art, sentences=2))
+        await ctx.send('Dear debater, here I my humble research on ' + wikipedia_research_art + " :art:")
+
+        try :
+            giphy_api_response = giphy_API_instance.gifs_search_get(giphy_API_key, wikipedia_research_art, limit=5)
+            movie_gif_list = list(giphy_api_response.data)
+            Movie_gif = random.choice(movie_gif_list)
+
+            emb = discord.Embed(title=wikipedia_research_art)
+            emb.set_image(url=f'https://media.giphy.com/media/{Movie_gif.id}/giphy.gif')
+            await ctx.send (embed=emb)
+
+        except ApiException as e:
+            await ctx.send ("nothing to see here, go through")
+
+        await ctx.send('Description : ' + wikipedia.summary(wikipedia_research_art, sentences=2))
+
+    except wikipedia.exceptions.PageError as a :
+
+        await ctx.send ("I am sorry my dear but this is out of my jurisdiction")
 
 
 @client.command(name="movie", help="helps you with movie")
 async def movie(ctx):
+
     User_message = ctx.message.content
     wikipedia_research_movie = User_message[3:]
-    await ctx.send('Dear debater, here I my humble research on the ' + wikipedia_research_movie + " :clapper:")
 
-    # add images with the poster find a way to get the right picture
-    await ctx.send(wikipedia.page(wikipedia_research_movie).images[1])
+    try :
 
-    # find how to access the little bando + add the director + date + main actor
+        await ctx.send('Dear debater, here I my humble research on the ' + wikipedia_research_movie + " :clapper:")
 
-    await ctx.send('Description : ' + wikipedia.summary(wikipedia_research_movie, sentences=1))
+        try :
+            giphy_api_response = giphy_API_instance.gifs_search_get(giphy_API_key, wikipedia_research_movie, limit=5)
+            movie_gif_list = list(giphy_api_response.data)
+            Movie_gif = random.choice(movie_gif_list)
 
-    # I cut the plot for it to be short enough (less than 4000 words) and also to not spoil other user
-    plot = wikipedia.page(wikipedia_research_movie).section('Plot')
-    plot_cut = plot[:200]
+            emb = discord.Embed(title=wikipedia_research_movie)
+            emb.set_image(url=f'https://media.giphy.com/media/{Movie_gif.id}/giphy.gif')
+            await ctx.send (embed=emb)
 
-    await ctx.send("Plot :   " + plot_cut + " .......  oh sorry I didn't meant to spoil, go look for yourself :exclamation:")
+        except ApiException as e:
+            await ctx.send ("nothing to see here, go through")
+
+        # find how to access the little bando + add the director + date + main actor
+
+        await ctx.send('Description : ' + wikipedia.summary(wikipedia_research_movie, sentences=2))
+
+        # I cut the plot for it to be short enough (less than 4000 words) and also to not spoil other user
+        plot = wikipedia.page(wikipedia_research_movie).section('Plot')
+        plot_cut = plot[:200]
+
+        await ctx.send("Plot :   " + plot_cut + " .......  oh sorry I didn't meant to spoil, go look for yourself :exclamation:")
+
+    except wikipedia.exceptions.PageError as a :
+
+        await ctx.send ("I am sorry my dear but this is out of my jurisdiction")
+
 
 
 
@@ -146,8 +181,6 @@ async def help(ctx):
     em.add_field(name = ("definition :book:"), value = "I will help you find the definition to your words. => use commands = 'al definition + your word' ")
     em.add_field(name = ("artwork :art:"), value = "I will help you find the artwork you are looking for. => use commands = 'al date + your artwork name' ")
     em.add_field(name = ("movie :clapper:"), value = "I will help you find the movie you are searching. => use commands = 'al date + your movie title' ")
-    em.add_field(name = "statistic", value = "I am still enquyring about this field, I shall come back soon enough to help you in your quest of knowledge")
-    em.add_field(name = "study", value = "I am still enquyring about this field, I shall come back soon enough to help you in your quest of knowledge")
     em.add_field(name = "funfact", value = "get a fun recap of a random subject")
     em.add_field(name = "need more help", value = "go to this link http : ")
 
