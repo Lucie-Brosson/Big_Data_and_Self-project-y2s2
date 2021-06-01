4ea3363eda17d1af54de530045299d5ad94823bc
Lucie-Maya <luciemayabrosson@gmail.com>
2021-06-01 19:24:09 +0100

    last version of the bot

--- final project - Al the debater/Al_the_debater.py
+++ final project - Al the debater/Al_the_debater.py
@@ -14,7 +14,7 @@ from giphy_client.rest import ApiException
 
 
 # API Gif giphy - to replace the image that I want I will you gif
-giphy_API_key = "put your api key here"
+giphy_API_key = "put your API key here"
 giphy_API_instance = giphy_client.DefaultApi()
 
 # Token to access discord
@@ -55,7 +55,7 @@ async def hello(ctx):
 @client.command(name="status", help="gives you his status")
 async def status(ctx):
 
-    status = ("Pretty good I should say","I... That is a good question, this is the first time someone ask. I would say better now that you asked","Terrible I have to say, I can't find the 3rd volume of my botanic encyclopedia")
+    status = ("Pretty good I should say", "I... That is a good question, this is the first time someone ask. I would say better now that you asked", "Terrible I have to say, I can't find the 3rd volume of my botanic encyclopedia")
 
     await ctx.send(random.choice(status))
 
@@ -107,7 +107,7 @@ async def artwork(ctx):
         await ctx.send('Dear debater, here I my humble research on ' + wikipedia_research_art + " :art:")
 
         try :
-            giphy_api_response = giphy_API_instance.gifs_search_get(giphy_API_key, wikipedia_research_art, limit=5)
+            giphy_api_response = giphy_API_instance.gifs_search_get(giphy_API_key, wikipedia_research_art, limit=2)
             movie_gif_list = list(giphy_api_response.data)
             Movie_gif = random.choice(movie_gif_list)
 
@@ -136,7 +136,7 @@ async def movie(ctx):
         await ctx.send('Dear debater, here I my humble research on the ' + wikipedia_research_movie + " :clapper:")
 
         try :
-            giphy_api_response = giphy_API_instance.gifs_search_get(giphy_API_key, wikipedia_research_movie, limit=5)
+            giphy_api_response = giphy_API_instance.gifs_search_get(giphy_API_key, wikipedia_research_movie, limit=2)
             movie_gif_list = list(giphy_api_response.data)
             Movie_gif = random.choice(movie_gif_list)
 
@@ -172,7 +172,7 @@ async def fun_fact(ctx):
 # personnalize help menu
 @client.group(invoke_without_command = True)
 async def help(ctx):
-    em = discord.Embed(title = "Glossary of my extended knwoledge", description ="use the 'al help + your subject of interest' to have more detail on how to use a specific command", color = dark_purple)
+    em = discord.Embed(title = "Glossary of my extended knwoledge", description ="use the 'al help + your subject of interest' to have more detail on how to use a specific command")
 
     em.add_field(name = "hello", value = "I will greet you")
     em.add_field(name = "status", value = "you will see")
@@ -185,10 +185,6 @@ async def help(ctx):
     em.add_field(name = "need more help", value = "go to this link http : ")
 
 
-    await ctx.send(embed =em)
-
-
-
-
+    await ctx.send(embed = em)
 
 client.run(TOKEN)
